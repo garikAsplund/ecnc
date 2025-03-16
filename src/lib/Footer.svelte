cd27e9b1fa65411e1c9de6a847da1895f9f16590
Garik Asplund <garik.asplund@gmail.com>
2025-03-15 20:26:58 -0700

    added watermark

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
 	import Facebook from './SVGs/Facebook.svelte';
+	import Watermark from './Watermark.svelte';
 </script>
 
 <footer
-	class="relative bottom-0 mt-auto flex w-full flex-col items-center justify-center space-y-8 md:space-y-0 border-t border-t-zinc-100 p-8 text-center"
+	class="relative bottom-0 mt-auto flex w-full flex-col items-center justify-center space-y-8 border-t border-t-zinc-100 pt-8 px-8 text-center md:space-y-0"
 >
 	<a
 		href="https://www.facebook.com/groups/1441033243294741"
 		target="_blank"
 		rel="noopener"
-		class="md:hidden hover:opacity-75 block"
+		class="block hover:opacity-75 md:hidden"
 	>
 		<Facebook />
 	</a>
@@ -21,4 +22,6 @@
 			class="underline underline-offset-2 hover:opacity-75">Oregon Nordic Club</a
 		>
 	</p>
+
+	<Watermark />
 </footer>
